f9493a53ebcc78591410e99aa9b1ca3762e761e4
adam25-adam06 <148205548+adam25-adam06@users.noreply.github.com>
2023-10-17 16:29:01 +0100

    Update challenge2.kt

--- src/main/kotlin/io/github/u/ways/challenge2.kt
+++ src/main/kotlin/io/github/u/ways/challenge2.kt
@@ -3,5 +3,7 @@ package io.github.u.ways
 import io.github.u.ways.domain.Request
 
 fun challenge2(request: Request) {
+  if ((!request.internet&&!request.tv) &&(!request.landline&&!request.mobile))   {
+       println("No products ordered")
 
-}
+}}
