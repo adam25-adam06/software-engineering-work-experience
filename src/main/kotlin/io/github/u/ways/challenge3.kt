2d2afb8dbb627172a206169a203d103fbd0aa40c
adam25-adam06 <148205548+adam25-adam06@users.noreply.github.com>
2023-10-18 14:16:25 +0100

    Update challenge3.kt

--- src/main/kotlin/io/github/u/ways/challenge3.kt
+++ src/main/kotlin/io/github/u/ways/challenge3.kt
@@ -3,4 +3,6 @@ package io.github.u.ways
 import io.github.u.ways.domain.Request
 
 fun challenge3(request: Request) {
+    if ((request.name== "")||(request.email== "")||(request.phone== "")||(request.address== ""))
+        println("A mandatory field is missing!")
 }
